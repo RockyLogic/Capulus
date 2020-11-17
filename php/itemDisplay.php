17494637eb6fbb4716b3999778c4f02056868707
Ze-hs <thehao33@gmail.com>
2020-11-17 17:05:14 -0500

    Added product preview and product display

---
+++ php/itemDisplay.php
@@ -0,0 +1,38 @@
+<?php
+$mysqli = new mysqli("", "", "", "");;
+if ($mysqli -> connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    exit();
+}
+
+
+$id = $_GET['id'];
+$query = "SELECT * FROM products WHERE id = $id;";
+$result = $mysqli->query($query);
+
+$row = $result->fetch_assoc();
+$name = $row["name"];
+$description = $row["description"];
+$price = $row["price"];
+$img_name = $row["image_name"];
+
+
+$itemInfo = <<<EOD
+  <div class="col-12 col-lg-6 itemDisplayImage" style="background-image: url('../images/$img_name');">
+  <!-- <img src="../images/travis1.jpg" alt="Jordan 1" height="100%" style="margin-left: auto;"> -->
+  </div>
+  <div class=" col-12 col-lg-6 d-flex flex-column justify-content-center" style="padding: 0 50px;">
+    <h1>$name</h1>
+    <h4>$$price</h4>
+    <br>
+    <h4>$Description</h4>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec finibus nibh vel leo aliquam molestie. Sed sit amet tellus metus. Aliquam erat volutpat. Mauris nunc justo.
+    </p>
+    <br>
+    <br>
+    <button class="btn btn-success">Add To Cart</button>
+    </div>
+EOD;
+    print($itemInfo);
+?>
---
+++ php/itemPreview.php
@@ -0,0 +1,41 @@
+<?php
+
+#Load the database
+#Enter database credentials
+$mysqli = new mysqli("", "", "", "");
+if ($mysqli -> connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    exit();
+}
+$query = "SELECT * FROM products;";
+$result = $mysqli->query($query);
+
+if ($result->num_rows > 0){
+  while($row = $result->fetch_assoc()){
+    $name = $row["name"];
+    $img_name = $row["image_name"];
+    $id = $row["id"];
+
+
+    $preview = <<<EOD
+
+    <div class="shop-item-box col-12 col-md-6 col-lg-4">
+      <div class="shop-item" style="background-image: url(' ../images/$img_name');">
+        <h5 class="shop-item-title">$name</h5>
+        <button onClick="redirect(this.id)" id="$id" class="btn btn-info" style="bottom:5%; right:45%; position: absolute;">View</a>
+        <button id="$id" class="btn btn-success" style="bottom:5%; right:10%; position: absolute;">Add To Cart</button>
+      </div>
+    </div>
+
+    <script type="text/javascript">
+      function redirect(id)
+      {
+          window.location.href = "itemDisplay.html?id=" + id;
+      }
+    </script>
+
+EOD;
+    print($preview);
+    }
+  }
+?>

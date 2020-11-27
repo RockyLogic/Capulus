452bd28d217f9bf396ed6c97321d24558d6ef39c
Rkuangdev <RKuangDev@gmail.com>
2020-11-27 09:46:57 -0500

    added animations on with intersection observer

---
+++ js/animations.js
@@ -0,0 +1,51 @@
+
+var fadeInSlideUp = document.querySelectorAll(".fadeInSlideUp")
+
+textObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.intersectionRatio > 0) {
+            entry.target.style.animation = `fadeInAndUp 1s forwards ease-out`
+        }
+    })
+})
+
+// Text elements fade in and move up
+fadeInSlideUp.forEach((element) => {
+    textObserver.observe(element)
+})
+
+
+var slideLeft = document.querySelectorAll(".slideLeft");
+
+sitesObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.intersectionRatio > 0) {
+            entry.target.style.animation = `slideLeft 1s ease-out`
+        }
+    })
+})
+
+// Text elements fade in and move up
+slideLeft.forEach((element) => {
+    sitesObserver.observe(element)
+})
+
+var slideRight = document.querySelectorAll(".slideRight")
+
+statObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.intersectionRatio > 0) {
+            entry.target.style.animation = `slideRight 1s ease-out`
+        }
+    })
+})
+
+// Text elements fade in and move up
+slideRight.forEach((element) => {
+    statObserver.observe(element)
+})
+
+//Scroll back to the top on reload
+// $(document).ready(function () {
+//     $(this).scrollTop(0);
+// });
